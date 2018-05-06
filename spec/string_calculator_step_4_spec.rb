f8bace2604a9e0c17976863d4b86830ae35eb054
Aaron Ciaghi <aaron.ciaghi@gmail.com>
2018-05-06 14:34:47 +0200

    another test for step 4

--- spec/string_calculator_step_4_spec.rb
+++ spec/string_calculator_step_4_spec.rb
@@ -5,5 +5,9 @@ describe StringCalculator, '#add' do
   context "when the default delimiter is ;" do
     it_behaves_like 'string calculator', "//;\n1;2;3", 6
     it_behaves_like 'string calculator', "//;\n1;2\n3", 6
+
+    it 'does not recognize , as delimiter' do
+      expect(StringCalculator.add("//;\n1,2;3")).not_to eq(3)
+    end
   end
 end
