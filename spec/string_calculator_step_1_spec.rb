b3ba9fe2d86a28cb34abab0feaf29fe4304f6fa6
Aaron Ciaghi <aaron.ciaghi@gmail.com>
2018-05-06 14:09:22 +0200

    step 1 done with 2 numbers

--- spec/string_calculator_step_1_spec.rb
+++ spec/string_calculator_step_1_spec.rb
@@ -22,4 +22,19 @@ describe StringCalculator, '#add' do
       expect(calculator.add('22')).to eq(22)
     end
   end
+
+  context "when the input is two numbers" do
+    it 'returns 3 with input "1,2"' do
+      expect(calculator.add('1,2')).to eq(3)
+    end
+
+    it 'returns 0 with input "0,0"' do
+      expect(calculator.add('0,0')).to eq(0)
+    end
+
+    it 'returns 222 with input "111,111"' do
+      expect(calculator.add('111,111')).to eq(222)
+    end
+
+  end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -3,7 +3,8 @@ class StringCalculator
   class << self
     # Splits a set of comma separated numbers and sums them
     def add(numbers)
-      numbers.to_i
+      numbers.split(',')
+             .inject(0) { |acc, num| acc += num.to_i }
     end
   end
 
