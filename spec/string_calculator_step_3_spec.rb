ffbec1608d3ca50ceb9720bc5475cfc839948449
Aaron Ciaghi <aaron.ciaghi@gmail.com>
2018-05-06 14:27:34 +0200

    step 3 done

---
+++ spec/string_calculator_step_3_spec.rb
@@ -0,0 +1,12 @@
+require './string_calculator'
+require './spec/shared_examples'
+
+describe StringCalculator, '#add' do
+  context "when the input uses \\n as delimiter" do
+    it_behaves_like 'string calculator', "1\n2\n3", 6
+  end
+
+  context "when the input uses both , and \\n as delimiter" do
+    it_behaves_like 'string calculator', "1\n2,3", 6
+  end
+end
--- string_calculator.rb
+++ string_calculator.rb
@@ -3,7 +3,7 @@ class StringCalculator
   class << self
     # Splits a set of comma separated numbers and sums them
     def add(numbers)
-      numbers.split(',')
+      numbers.split(/,|\n/)
              .inject(0) { |acc, num| acc += num.to_i }
     end
   end
