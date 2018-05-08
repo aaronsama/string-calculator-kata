12e1e237791fb99d6334cd0604b5d42113d13744
Aaron Ciaghi <aaron.ciaghi@gmail.com>
2018-05-08 15:34:39 +0200

    refactoring after step 1

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
+function sumReducer(accumulator, number) {
+  return accumulator + parseInt(number);
+}
+
 function add(numbers) {
   if (numbers.length === 0) {
     return 0
   } else {
     const splitNumbers = numbers.split(',')
-    return splitNumbers.reduce((accumulator, number) => {
-      return accumulator + parseInt(number);
-    }, 0);
+    return splitNumbers.reduce(sumReducer, 0);
   }
 }
 
