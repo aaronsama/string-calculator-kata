81cb71057b79a96fe9cb4c207aeecda361f79c40
Aaron Ciaghi <aaron.ciaghi@gmail.com>
2018-05-08 15:08:13 +0200

    using chai so it's similar to rspec

--- js/test/step1_test.js
+++ js/test/step1_test.js
@@ -1,9 +1,9 @@
 const { describe, it } = require('mocha');
-const assert = require('assert');
+const { expect } = require('chai');
 const add = require('../');
 
 describe('add', function() {
   it('returns 0 with empty input', function() {
-    assert.equal(add(''), 0);
+    expect(add('')).to.be.equal(0)
   })
 });
